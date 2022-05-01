05a0b54d86d80214da08280c6d172d3bebccdb82
Scott Fillmer <scottfillmer@me.com>
2022-05-01 10:45:20 -0500

    Changes up to lesson 7

---
+++ src/Navbar.js
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return ( 
+        <div><nav className="navbar"></nav><h1>The Dojo Blog</h1><div className="links">
+            <a href="/">Home</a>
+            <a href="/create">New Blog</a>
+        </div></div>
+     );
+}
+ 
+export default Navbar;
